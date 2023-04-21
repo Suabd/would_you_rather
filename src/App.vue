744617491cc4793f71ab3c592e11fe5b540d6dab
Your Name <you@example.com>
2023-04-21 14:56:48 -0500

    lab

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,110 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="App">
+  
+      <h1> Would You Rather?</h1>
+      
+      <would-you-rather v-bind:key="question.id" 
+        v-for="question in questions"
+        v-bind:id="question.id"
+        v-bind:question="question.wyrQuestion"
+        v-bind:answer1="question.wyrAnswer1"
+        v-bind:answer2="question.wyrAnswer2"
+        v-on:answer-changed="answerChanged">
+        </would-you-rather> 
+      
+      <div id="results">
+      <!-- this will be the header of the results-->
+      <h1 > You would rather ... </h1>
+      <!-- this will be shown before the list of user choices-->
+      <p id="userSelection" v-show="!choices"> Try Making a selection above?</p> 
+      
+      <!--- create list of user choices for the qustions-->
+        <ul id='userAnswer'>
+        <li v-for="message in userSelectionMessages" v-bind:key="message"> 
+          {{ message }}</li>
+        </ul>
+      </div>
+    </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WouldYouRather from './components/WouldYouRather.vue'
 
-export default {
+export default { 
   name: 'App',
   components: {
-    HelloWorld
+    WouldYouRather
+  },
+
+  data(){
+    return{
+      // array of questions object, threeor more questions
+      //each question object has an id, we'll have 3 wouldyourather
+      //components and each will emit a message when the user makes choice
+      //so if we give each coby of wouldyourather a unique ID,then
+      //that component can include the ID when it emit a message
+      //and  app.vue will be able to tell which component emitted that message.
+      questions: [ 
+        {
+          id: 0,
+        wyrQuestion: ' ... get up early or stay up late',
+        wyrAnswer1: 'Early',
+        wyrAnswer2: 'Late',
+       
+        },
+        {
+          id: 1,
+          wyrQuestion: ' ... do school work as a group or by yourself?',
+          wyrAnswer1: 'As a group',
+          wyrAnswer2: 'By yourself',
+         
+        },
+        {
+          id: 2,
+          wyrQuestion: ' ... do a book report or a science project for a school assignment?',
+          wyrAnswer1: 'Book report',
+          wyrAnswer2: 'Science project',
+         
+        },
+      ],
+      // user selections message? or messages? or ???
+      userSelectionMessages: [], // insted of a string with one message, what could we do?
+    }
+  }, 
+  methods: { // this will show the user choice shoing only one answer for each id
+    answerChanged(choice, wyrComponentId){ //
+      console.log('This is answer changed', choice, wyrComponentId)
+      this.userSelectionMessages[wyrComponentId] = choice // shows selected answer 
+    }
   }
 }
 </script>
 
 <style>
+
+body {
+  background: lightskyblue;;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Arial, Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 40px;
 }
+
+li {
+  color: navy;
+  list-style: square;
+  list-style-position: inside;
+  font-size: 1.3em;
+  font-family: Arial, Helvetica, sans-serif;
+
+}
+
+
+
 </style>
---
+++ src/components/WouldYouRather.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="wyr">
+   
+    <h2>  Would You Rather ..... </h2>
+
+    <h3> {{ question }}</h3>
+
+    <input type="radio" v-model="choice" v-bind:value="answer1" v-on:change="choiceMade">
+    <label>{{ answer1 }}</label>
+    <input type="radio" v-model="choice" v-bind:value="answer2" v-on:change="choiceMade">
+    <label>{{ answer2 }}</label>
+
+  </div>
+</template> 
+
+<script>
+export default {
+  name: 'WouldYouRather',
+  props: {
+    id: Number,
+    question: String,
+    answer1: String,
+    answer2: String,
+  },
+  data(){
+    return{
+      choice:''
+    }
+  },
+  methods: { 
+    choiceMade(){ // will should what ever user chice from radio
+      // emit an called 'answer-changed' 
+      //and  include the value of the data choice
+      //and this component's  id 
+      this.$emit('answer-changed', this.choice, this.id)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 30px 0 0;
+}
+
+.wyr {
+  border: 2px  rgb(3, 0, 14) solid;
+  background: rgb(250, 248, 143);
+  margin: 5px;
+  padding: 10px;
+  text-align: left;
+}
+
+
+
+
+</style>
